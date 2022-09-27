6c1f51c815eecbc678f506bd633c7a7756289bb4
fyzanshaik <fyzan.shaik@gmail.com>
2022-09-27 13:05:56 +0530

    2 new files have been added

---
+++ data_conversion.py
@@ -0,0 +1,4 @@
+birth_year = input("What is your birth year? ")
+print("You were born on " + birth_year)
+age = 2022 - int(birth_year)
+print('You are ' + str(age) + " years.")
---
+++ weight_conversion.py
@@ -0,0 +1,3 @@
+your_weight_in_kg = input('What is your weight in kg? ')
+your_weight_in_pounds = float(your_weight_in_kg) * 2.20462
+print("You weigh " + str(your_weight_in_pounds) + 'pounds')
