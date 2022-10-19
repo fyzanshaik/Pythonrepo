42260f790c7a2977c7fbd8f754363b27daebd147
fyzanshaik <fyzan.shaik@gmail.com>
2022-10-19 12:46:42 +0530

    New file added

---
+++ Conditional + Boolean Logic/favanime.py
@@ -0,0 +1,12 @@
+print("What is the best anime?")
+anime = input()
+if anime == ("Full metal alchemist"):
+    print("You have the best choice!")
+elif anime == ('Code geass'):
+    print("Not bad, Good choice!")
+elif anime == ('Mob psycho 100'):
+    print("Amazing!")
+elif anime == ('Chainsaw man'):
+    print("Love the manga can't wait for anime to finish!!")
+else:
+    print("Lol you have bad taste, kill yourself!!")
